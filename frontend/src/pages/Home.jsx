795e220a495de373fbfb9ce4c0d98846915218db
Niraj Gupta <coder4117@gmail.com>
2025-04-20 20:09:18 +0530

    steps component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
+import Steps from '../components/Steps'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
         <Header/>
+        <Steps/>
     </div>
   )
 }
