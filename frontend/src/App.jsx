65bc1369d2d1ed1c9214d417b415e024fa8afe4a
Niraj Gupta <coder4117@gmail.com>
2025-04-20 18:56:02 +0530

    navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import {Routes, Route} from "react-router-dom";
+import Home from './pages/Home';
+import Result from './pages/Result';
+import Pricing from './pages/Pricing';
 
 function App() {
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/result' element={<Result/>}/>
+        <Route path='/pricing' element={<Pricing/>}/>
+      </Routes>
+    </div>
   )
 }
 
