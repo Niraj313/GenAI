3e96b1e9917b95579a0b0aa542bc48a666dfbd91
Niraj Gupta <coder4117@gmail.com>
2025-04-24 17:55:13 +0530

    frontend complete

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,21 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Header from '../components/Header'
-import Steps from '../components/Steps'
-import Description from '../components/Description'
-import Testimonial from '../components/Testimonial'
-import GenerateBtn from '../components/GenerateBtn'
-import Footer from '../components/Footer'
-import Login from '../components/Login'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import Steps from "../components/Steps";
+import Description from "../components/Description";
+import Testimonial from "../components/Testimonial";
+import GenerateBtn from "../components/GenerateBtn";
 
 const Home = () => {
   return (
     <div>
-        {/* <Navbar/>
-        <Header/>
-        <Steps/>
-        <Description/>
-        <Testimonial/>
-        <GenerateBtn/>
-        <Footer/> */}
-        <Login/>
-        
+      <Header />
+      <Steps />
+      <Description />
+      <Testimonial />
+      <GenerateBtn />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
